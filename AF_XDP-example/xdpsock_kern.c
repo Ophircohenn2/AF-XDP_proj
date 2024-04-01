15b4bb9fd7742f6d86eacf5b8751ddedede376fb
nirtevet <nir.tevet@campus.technion.ac.il>
2024-04-01 11:08:16 +0300

    kernel code cleaning

--- AF_XDP-example/xdpsock_kern.c
+++ AF_XDP-example/xdpsock_kern.c
@@ -3,16 +3,15 @@
 #include <bpf/bpf_helpers.h>
 #include "xdpsock.h"
 
-/* This XDP program is only needed for multi-buffer and XDP_SHARED_UMEM modes.
- * If you do not use these modes, libbpf can supply an XDP program for you.
- */
 
+//debug function that allow us to print variables to the kernel log
 #define odbpf_vdebug(fmt, args...)                                                       \
 	({                                                                                   \
 		char ____fmt[] = fmt;                                                            \
 		bpf_trace_printk(____fmt, sizeof(____fmt), ##args);                              \
 	})
 
+//ebpf map that hold pointers to the AF-XDP sockets
 struct {
 	__uint(type, BPF_MAP_TYPE_XSKMAP);
 	__uint(max_entries, MAX_SOCKS + 1);
@@ -20,26 +19,14 @@ struct {
 	__uint(value_size, sizeof(int));
 } xsks_map SEC(".maps");
 
-// int num_socks = 16;
-// static unsigned int rr = 0;
 static u32 index = 0;
 SEC("xdp_sock")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
-	// return XDP_PASS;
+
+
     index = ctx->rx_queue_index;
-    // index=rr%num_socks;
-    // rr++;
-	// odbpf_vdebug("index = %d\n", index);
-    // __u32 *pkt_count;
 
-    // pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-    // if (pkt_count) {
-
-    //     /* We pass every other packet */
-    //     if ((*pkt_count)++ & 1)
-    //         return XDP_PASS;
-    // }
 
     /* A set entry here means that the correspnding queue_id
      * has an active AF_XDP socket bound to it. */
